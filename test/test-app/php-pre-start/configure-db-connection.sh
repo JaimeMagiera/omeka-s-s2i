8dbcdfd005bc961a146a639eacb59201d27db55e
JaimeMagiera <jaimelm@umich.edu>
2020-11-16 14:44:56 -0500

    quote that shit

--- test/test-app/php-pre-start/configure-db-connection.sh
+++ test/test-app/php-pre-start/configure-db-connection.sh
@@ -8,8 +8,8 @@
 # ;log_path = 
 
 CONFIG_FILE=/opt/app-root/src/omeka-s/config/database.ini
-echo "user = ${db_user}" > ${CONFIG_FILE}
-echo "password = ${db_password}" >> ${CONFIG_FILE}
-echo "dbname = ${db_name}" >> ${CONFIG_FILE}
-echo "host = ${db_host}" >> ${CONFIG_FILE}
+echo 'user = "${db_user}"' > ${CONFIG_FILE}
+echo 'password = "${db_password}"' >> ${CONFIG_FILE}
+echo 'dbname = "${db_name}"' >> ${CONFIG_FILE}
+echo 'host = "${db_host}"' >> ${CONFIG_FILE}
 
