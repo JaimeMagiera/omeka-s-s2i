b0c9cb3814f174aa551c988c68b121a0467db0df
JaimeMagiera <jaimelm@umich.edu>
2020-11-16 14:33:52 -0500

    herpdederp

--- test/test-app/php-pre-start/configure-db-connection.sh
+++ test/test-app/php-pre-start/configure-db-connection.sh
@@ -9,7 +9,7 @@
 
 
 echo "user = ${db_user}" > /opt/app-root/src/omeka-s/config
-echo "password = ${db_password}" > /opt/app-root/src/omeka-s/config
-echo "dbname = ${db_name}" > /opt/app-root/src/omeka-s/config
-echo "host = ${db_host}" > /opt/app-root/src/omeka-s/config
+echo "password = ${db_password}" >> /opt/app-root/src/omeka-s/config
+echo "dbname = ${db_name}" >> /opt/app-root/src/omeka-s/config
+echo "host = ${db_host}" >> /opt/app-root/src/omeka-s/config
 
