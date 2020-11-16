dfb2636d4db4a679d35db42513258cd8a9032f2f
JaimeMagiera <jaimelm@umich.edu>
2020-11-16 16:36:25 -0500

    using config variables

--- test/test-app/php-pre-start/configure-db-connection.sh
+++ test/test-app/php-pre-start/configure-db-connection.sh
@@ -8,8 +8,8 @@
 # ;log_path = 
 
 CONFIG_FILE=/opt/app-root/src/omeka-s/config/database.ini
-echo -e "user = \"${db_user}\"" > ${CONFIG_FILE}
-echo -e "password = \"${db_password}\"" >> ${CONFIG_FILE}
-echo -e "dbname = \"${db_name}\"" >> ${CONFIG_FILE}
+echo -e "user = \"${database-user}\"" > ${CONFIG_FILE}
+echo -e "password = \"${database-password}\"" >> ${CONFIG_FILE}
+echo -e "dbname = \"${database-name}\"" >> ${CONFIG_FILE}
 echo -e "host = \"${db_host}\"" >> ${CONFIG_FILE}
 
